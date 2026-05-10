e4e791276aba6af14a03f6b70d2b8695d77bacc0
S-a-t-y-aa-y-t-a-S <satyasish07@gmail.com>
2026-05-10 19:08:36 +0530

    DSA-1 In Progress C file updated

--- avl.C
+++ avl.C
@@ -291,9 +291,6 @@ void deleteNode(struct node** rootNode, int key) {
         
         free(succNode);
         succNode = 0;
-        printf ("Hello world\n");
-        printf ("Hello world again\n");
-        printf ("again and again\n");
 
         // need to fix this bug
         checkForRotation(rootNode, 0, pop(&st1), &st1, st1.top+1, 0);
