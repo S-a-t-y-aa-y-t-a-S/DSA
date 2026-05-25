7adde1be8100d8144f8ff5ca9662baf644b5cd4b
S-a-t-y-aa-y-t-a-S <satyasish07@gmail.com>
2026-05-25 14:38:25 +0530

    DSA-5 #in-progress need to update the code fragment between the existing parent node and the sibling node connection

--- 2_3_tree.C
+++ 2_3_tree.C
@@ -48,41 +48,99 @@ void insertion(struct node** rootNode, int key) {
     else {
         struct node* parentNode = 0;
         struct node* siblingNode = 0;
+        int flag = 0;
 
         while(!isEmptyStack(st)) {
-            struct node* poppedNode = pop(&st);
-            int temp = key;
             
-            if (poppedNode->data1 && poppedNode->data2) {
-                siblingNode = createNode();
-
-                if (!stackTop(st) || stackTop(st)->data2) {
-                    parentNode = createNode(); 
-                }
+            struct node* currentNode = pop(&st);
+            if (!isEmptyStack(st) && stackTop(st)->data2) {
+                // meaning if no parent node exists or the parent node is full
+                // create a new parent node
+                parentNode = createNode();
+                push(&st, parentNode);
+            }
+            if (currentNode->data1 && currentNode->data2) {
+                // meaning  the no space to insert the key in the current node
+                // popped out from the stack, representing the traversal
+                if (!siblingNode)
+                    siblingNode = createNode();
                 
-                if (key < poppedNode->data1) {
-                    siblingNode->data1 = temp;
-
+                if (parentNode) {
+                    // the newly created parent node is assigned its child and the value
+                    // the value can be the key or the already existing value shifted up depending 
+                    // upon the condition applied
+                    if (stackTop(st)->leftChild == currentNode) {
+                        stackTop(st)->leftChild = 0;
+                    }   
+                    else if (stackTop(st)->midChild == currentNode) {
+                        stackTop(st)->midChild = 0;
+                    } 
+                    else if (stackTop(st)->rightChild == currentNode) {
+                        stackTop(st)->rightChild = 0;
+                    }
+                    if (key < currentNode->data1) {
+                        parentNode->data1 = currentNode->data1;
+                        parentNode->leftChild = siblingNode;
+                        parentNode->midChild = currentNode;
+                        siblingNode->data1 = key;
+                        currentNode->data1 = currentNode->data2;
+                        currentNode->data2 = 0;
+                    }
+                    else if (key > currentNode->data1 && key < currentNode->data2) {
+                        parentNode->data1 = key;
+                        parentNode->leftChild = currentNode;
+                        parentNode->midChild = siblingNode;
+                        siblingNode->data1 = currentNode->data2;
+                        currentNode->data2 = 0;
+                    }
+                    else if (key > currentNode->data2) {
+                        parentNode->data1 = currentNode->data2;
+                        parentNode->leftChild = currentNode;
+                        parentNode->midChild = siblingNode;
+                        siblingNode->data1 = key;
+                        currentNode->data2 = 0;
+                    }
+                    key = parentNode->data1;
                 }
-                else if (key > poppedNode->data1 && key < poppedNode->data2) {
-                    siblingNode->data1 = poppedNode->data2;
-                    temp = key;
+                else {
+                    // need to fix the issue/bug
+                    // need to apply the sibling node over here
+                    int temp = 0;
+                    // ##make some changes in how does the stack top shall now be connected to the current node
+                    if (key < currentNode->data1) {
+                        currentNode->data1 = key;
+                        temp = currentNode->data1;
+                        siblingNode->data1 = currentNode->data2;
+                        
+                    }
+                    else if (key > currentNode->data1 && key < currentNode->data2) {
+                        temp = key;
+                        siblingNode->data1 = currentNode->data2;
+                    }
+                    else if (key > currentNode->data2) {
+                        siblingNode->data1 = key;
+                        temp = currentNode->data2;
+                    }
+                    currentNode->data2 = 0;
+                    stackTop(st)->data2 = temp;
+                    
+                    flag = 1;
                 }
-                else if (key > poppedNode->data2) {
-                    siblingNode->data1 = temp;
-                    key = poppedNode->data2;
-                    poppedNode->data2 = 0;
-
-                }
+                // need to transfer the child node of the newly created node to the immediate parent node
+                // newly created node then becomes the root node or the parent of the immediate parent or the thing will keep 
+                // on repeating
             }
             else {
-                if (poppedNode->data1)  
-                    poppedNode->data2 = key;
-                else {
-                    poppedNode->data2 = poppedNode->data1;
-                    poppedNode->data1 = key;
+                if (key < currentNode->data1) {
+                    currentNode->data2 = currentNode->data1;
+                    currentNode->data1 = key;
                 }
+                else
+                    currentNode->data2 = key;
+                flag = 1;
             }
+            if (flag)
+                break;
         }
     }
 
